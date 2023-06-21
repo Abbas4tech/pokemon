7e11b10b56e3cbb7258f94ec6db1d9fa219fe9c4
Abbas4tech <shaikhabbas54@gmail.com>
2023-06-21 12:02:28 +0530

    basic ui packages added

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+      .then((res) => res.json().then((data) => console.log(data)))
+      .catch((e) => console.log(e));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className={" h-20 bg-gray-900 text-white "}>
+        Abbas
+        <button className="btn btn-primary">Submit</button>
       </header>
-    </div>
+    </>
   );
 }
 
